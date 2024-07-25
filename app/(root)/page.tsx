d28a9259bb2cc51de29c7863a376c4eb74b6fa94
TedrosTK <tedydada@gmail.com>
2024-07-25 16:21:27 -0400

    sidebar and mobilenav

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 
 const Home = () => {
 
-    const loggedIn = {firstName: 'Adrian'}
+    const loggedIn = {firstName: 'Tedros'}
   return (
     <section className="home">
         <div className="home-content">
