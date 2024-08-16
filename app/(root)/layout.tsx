748c3fc6f5a00fcf8a4054d1b06fb283daa14f9a
TedrosTK <tedydada@gmail.com>
2024-08-16 16:07:11 -0400

    app modified

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,5 +1,5 @@
 import MobileNav from "@/components/MobileNav";
-import SideBar from "@/components/SideBar";
+import Sidebar from "@/components/SideBar";
 import { getLoggedInUser } from "@/lib/actions/user.actions";
 import Image from "next/image";
 import { redirect } from "next/navigation";
@@ -9,26 +9,23 @@ export default async function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-
- 
   const loggedIn = await getLoggedInUser();
 
-  if (!loggedIn) redirect('/sign-in')
+  if(!loggedIn) redirect('/sign-in')
+
   return (
     <main className="flex h-screen w-full font-inter">
-       <SideBar user={loggedIn} />
-       <div className="flex size-full flex-col">
+      <Sidebar user={loggedIn} />
+
+      <div className="flex size-full flex-col">
         <div className="root-layout">
-          <Image 
-            src="/icons/logo.svg"
-            width={30}
-            height={20}
-            alt="menu icon"
-          />
-          <MobileNav user={loggedIn}/>
+          <Image src="/icons/logo.svg" width={30} height={30} alt="logo" />
+          <div>
+            <MobileNav user={loggedIn} />
+          </div>
         </div>
         {children}
-       </div>
+      </div>
     </main>
   );
 }
